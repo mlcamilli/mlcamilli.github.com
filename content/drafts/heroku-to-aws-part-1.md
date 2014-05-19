cfef92ae9edaca63849428e23888c881b689ebc3
Matt Camilli <mlcamilli@gmail.com>
2014-05-19 13:10:24 -0400

    added new article feature as well as a command to list drafts and promote drafts

---
+++ content/drafts/heroku-to-aws-part-1.md
@@ -0,0 +1,7 @@
+Title: Heroku to AWS part 1
+Date: 2014-05-19 01:09 PM
+Category: 
+Tags: 
+Slug: heroku-to-aws-part-1
+Author: Matt Camilli
+Description: 
--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,10 @@
-from fabric.api import *
+from fabric.api import local, env, hosts
 import fabric.contrib.project as project
+from datetime import datetime
+from pytz import timezone
+import shutil
 import os
+
 
 # Local path configuration (can be absolute or relative to fabfile)
 env.deploy_path = 'output'
@@ -21,25 +25,32 @@ def clean():
         local('rm -rf {deploy_path}'.format(**env))
         local('mkdir {deploy_path}'.format(**env))
 
+
 def build():
     local('pelican -s pelicanconf.py')
+
 
 def rebuild():
     clean()
     build()
 
+
 def regenerate():
     local('pelican -r -s pelicanconf.py')
 
+
 def serve():
     local('cd {deploy_path} && python -m SimpleHTTPServer'.format(**env))
+
 
 def reserve():
     build()
     serve()
 
+
 def preview():
     local('pelican -s publishconf.py')
+
 
 def cf_upload():
     rebuild()
@@ -48,6 +59,31 @@ def cf_upload():
           '-U {cloudfiles_username} '
           '-K {cloudfiles_api_key} '
           'upload -c {cloudfiles_container} .'.format(**env))
+
+
+def new(article_name):
+    slug = article_name.lower().replace(' ', '-')
+    filename = slug + '.md'
+    with open('content/drafts/{}'.format(filename), 'w') as writer:
+        writer.write('Title: {}\n'.format(article_name))
+        writer.write('Date: {}\n'.format(timezone('US/Eastern').localize(
+            datetime.now()).strftime('%Y-%m-%d %I:%M %p')))
+        writer.write('Category: \n')
+        writer.write('Tags: \n')
+        writer.write('Slug: {}\n'.format(slug))
+        writer.write('Author: Matt Camilli\n')
+        writer.write('Description: \n')
+
+
+def drafts():
+    drafts = sorted(os.listdir('content/drafts/'))
+    for count, draft in enumerate(drafts):
+        print '{}: {}'.format(count, draft)
+
+def finish(draft_number):
+    drafts = sorted(os.listdir('content/drafts/'))
+    draft = drafts[int(draft_number)]
+    shutil.move('content/drafts/{}'.format(draft), 'content/{}'.format(draft))
 
 
 def push():
